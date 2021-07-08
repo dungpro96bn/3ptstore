07ef2f28bf5b91de33faeb90e8931697d3bb711f
ヴイ マン　ズン <b1dung@sg.sougo-staff.co.jp>
2021-07-08 16:45:04 +0700

    fix css

--- wp-content/themes/zigcy-lite-child/assets/js/custom.js
+++ wp-content/themes/zigcy-lite-child/assets/js/custom.js
@@ -5,4 +5,13 @@ jQuery(function ($) {
         $(".woocommerce-ordering select").chosen({disable_search_threshold: 100});
     });
 
+    $(document).ajaxStop(function() {
+        $(".facetwp-facet-danh_mc .facetwp-dropdown").chosen({disable_search_threshold: 100});
+    });
+
+    $(".woocommerce-pagination li a").click(function () {
+        var href = $(this).attr('href');
+        window.location.replace(href);
+    });
+
 });
